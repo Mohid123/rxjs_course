31c3c0012350e99523630f4488f590ff07df484d
Mohid <mohid.dastgeer@solissol.com>
2021-12-22 16:36:08 +0500

    Main Commit

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { interval, timer, Observable, noop, of, concat } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'about',
@@ -10,6 +12,147 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-  }
+
+    // //What is a stream? everything is asynchronous in JS and all these are streams of eventss
+    // //Click events on the page are streams, never complete
+    // document.addEventListener('click', evt => {
+    //   console.log(evt)
+    // });
+
+    // // intervals are also streams- emits continuously, never completes
+    // let counter = 0;
+    // setInterval(() => {
+    //   console.log(counter);
+    //   counter++;
+    // }, 2000);
+
+    // //calling setTimeout - emits value only once and completes
+
+    // setTimeout(() => {
+    //   console.log('finished...')
+    // }, 3000)
+
+// Lesson2: Combining values results in a nested callback like below
+
+    // document.addEventListener('click', evt => {
+    //   console.log(evt)
+    //   setTimeout(() => {
+    //     let counter = 0;
+    //     setInterval(() => {
+    //       console.log(counter);
+    //       counter++;
+    //     }, 2000);
+    //     console.log('finished...')
+    //   }, 3000)
+    // });
+
+//Lesson 3-  What is an observable?
+
+    // const interval$ = interval(1000); //defining an observable. dollar sign is used to denote an observable. The observable parametrically gets type number
+    // interval$.subscribe(val => console.log('stream 1 => ' + val)); //subscribe means a stream of values is created
+    // interval$.subscribe(val => console.log('stream 2 => ' + val)); //observable can be subscribed multiple times with both subscriptions emitting values seperately
+
+    // const interval$ = timer(3000, 1000);
+    // interval$.subscribe(val => console.log('stream 1 => ' + val));
+
+    // const click$ = fromEvent(document, 'click');
+    // click$.subscribe(val => console.log(val));
+
+// Lesson 4 - Subscribe methods basics
+
+    // const click$ = fromEvent(document, 'click');
+    // const sub = click$.subscribe(
+    //   val => console.log(val),
+    //   err => console.log(err), //observables either error out or complete after they emit all their values
+    //   () => console.log('completed')
+    //   );
+
+    //   setTimeout(() => {
+    //     sub.unsubscribe()
+    //   }, 5000)
+
+// Lesson 5 - Create your own custom http observable
+
+  // fetch('/api/courses'); //http call to backend in the form of a promise
+
+  // const http$ =  Observable.create(observer => {
+  //   fetch('/api/courses').then(response => {
+  //     return response.json()
+  //   })
+  //   .then(body => {
+  //     observer.next(body);
+  //     observer.complete();
+  //   })
+  //   .catch(err => {
+  //     observer.error(err);
+  //   })
+  // })
+
+  // http$.subscribe(
+  //   courses => console.log(courses),
+  //   noop, //no operation operator fromn rxjs
+  //   () => console.log('completed')
+  // );
+
+// Lesson 6 - The map operator
+  // create a seperate function for the http get req for convenience
+function createHttpObservable(url: string) {
+  return Observable.create(observer => {
+    fetch('/api/courses').then(response => {
+      return response.json()
+    })
+    .then(body => {
+      observer.next(body);
+      observer.complete();
+    })
+    .catch(err => {
+      observer.error(err);
+    })
+  })
+}
+
+const http$ = createHttpObservable('/api/courses');
+
+const courses$ = http$.pipe(
+  map(
+    res => Object.values(
+      res['payload'])//object.values converts to array. The output is an observable that emits an array of courses.
+      ) //The map operator maps the incoming observable into any other value we want to manipulate or transform it to.
+);
+
+courses$.subscribe(courses => {
+  console.log(courses),
+  noop,
+  () => console.log('completed')
+})
+
+// Lesson 7 - Seperating the view of advanced and beginner courses
+// Go to home component for lesson 7
+
+//Lesson 9
+
+//Concat Operator
+
+  //const source1$ = of(1, 2, 3);
+//   const source1$ = interval(1000)
+//   const source2$ = of(4, 5, 6);
+//   const source3$ = of(7, 8, 9);
+
+//   // we want to combine them together. But the result should be sequential.
+//   //i.e source1 should complete first and then source2
+
+// const result$ = concat(source1$, source2$, source3$)
+
+// result$.subscribe(val => console.log(val)); //all concatenated together
+
+
+// Lesson 10
+
+//concatMap()
+
+
+
+}
+
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Course} from "../model/course";
-import {interval, Observable, of, timer} from 'rxjs';
-import {catchError, delayWhen, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
+import {interval, noop, Observable, of, timer} from 'rxjs';
+import { catchError, delayWhen, map, retryWhen, shareReplay, tap, filter } from 'rxjs/operators';
 
 
 @Component({
@@ -11,6 +11,15 @@ import {catchError, delayWhen, map, retryWhen, shareReplay, tap} from 'rxjs/oper
 })
 export class HomeComponent implements OnInit {
 
+  //From Lesson 8 - better approach
+  beginnersCourse$: Observable<Course[]>;
+  advancedCourse$: Observable<Course[]>;
+
+
+ // From Lesson 7
+  beginnersCourse: Course[];
+  advancedCourse: Course[];
+
 
     constructor() {
 
@@ -18,8 +27,59 @@ export class HomeComponent implements OnInit {
 
     ngOnInit() {
 
+      // Lesson 7 - Seperating the view of advanced and beginner courses
+
+      const http$ = this.createHttpObservable('/api/courses');
+
+      // From Lesson 7
+
+      const courses$: Observable<Course[]> = http$.pipe(
+        tap(() => console.log("Http Request Executed")), //tap is used for emitting side effects outside the observable stream i.e for errors or consoling etc.
+        map(
+          res => Object.values(
+            res['payload'])
+            ),
+            shareReplay()
+          );
+
+      // Lesson 8 - continues from 7
+      this.beginnersCourse$ = courses$.pipe(
+        map(
+          courses => courses.filter(x => x.category == 'BEGINNER')
+          )
+      );
+
+      this.advancedCourse$ = courses$.pipe(
+        map(courses => courses.filter(x => x.category == 'ADVANCED')
+        )
+      );
 
 
+
+      // From Lesson 7 - Old approach
+
+      // courses$.subscribe(courses => {
+      //   this.beginnersCourse = courses.filter(x => x.category == 'BEGINNER'),
+      //   this.advancedCourse = courses.filter(x => x.category == 'ADVANCED'),
+      //   noop,
+      //   () => console.log('completed')
+      // })
+
+    }
+/// Our custom observable
+    createHttpObservable(url: string) {
+      return Observable.create(observer => {
+        fetch('/api/courses').then(response => {
+          return response.json()
+        })
+        .then(body => {
+          observer.next(body);
+          observer.complete();
+        })
+        .catch(err => {
+          observer.error(err);
+        })
+      })
     }
 
 }
